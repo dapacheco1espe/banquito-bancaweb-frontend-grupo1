984486b324550a2dffc3407deeef525bcded33d9
Alexander Pacheco <dapacheco1@espe.edu.ec>
2023-07-09 21:37:31 -0500

    Revert "Transferencias with Styles"
    This reverts commit 639e87f8084de53cfebdf6774239a5754430daee.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -65,11 +65,6 @@ export const appRoutes: Route[] = [
             {
                 path : 'client',
                 loadChildren: () => import('app/modules/client/client.module') .then(m => m.ClientModule),
-    
-            },
-            {
-                path: 'transferencias',
-                loadChildren: () => import('app/modules/client/account-transaction/account-transaction.module').then(m => m.AccountTransactionModule),
             }
         ]
     }
