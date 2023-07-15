957aba49f642857548774b3dd021feb5de1ee840
BrAvalos <65546785+BrAvalos@users.noreply.github.com>
2023-07-15 12:53:45 -0500

    Add files via upload

--- src/app/mock-api/index.ts
+++ src/app/mock-api/index.ts
@@ -22,6 +22,7 @@ import { ScrumboardMockApi } from 'app/mock-api/apps/scrumboard/api';
 import { ShortcutsMockApi } from 'app/mock-api/common/shortcuts/api';
 import { TasksMockApi } from 'app/mock-api/apps/tasks/api';
 import { UserMockApi } from 'app/mock-api/common/user/api';
+import { ClientAccountsMockApi } from './client/accounts/api';
 
 export const mockApiServices = [
     AcademyMockApi,
@@ -47,5 +48,7 @@ export const mockApiServices = [
     ScrumboardMockApi,
     ShortcutsMockApi,
     TasksMockApi,
-    UserMockApi
+    UserMockApi,
+    //new apis
+    ClientAccountsMockApi,
 ];
