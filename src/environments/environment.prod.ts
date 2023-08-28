f64d75beffa3968d541c595ac137f58b08e27b91
BrAvalos <b.r-avalos@hotmail.com>
2023-08-28 18:08:14 -0500

    Cambio url apigateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
     production: true,
-    urlApiAccount:'https://banquito-accounts-try5-zsorzzk5da-uc.a.run.app/api/v1/accounts',
-    urlApiAccountProduct: 'https://banquito-ws-passive-products-zsorzzk5da-uc.a.run.app/api/v1/productAccount',
-    urlApiAccountTransaction: 'https://banquito-accounts-try5-zsorzzk5da-uc.a.run.app/api/v1/transactions',
+    urlApiAccount:'https://apiv18-62gh6lrj.uc.gateway.dev/api/v1/accounts',
+    urlApiAccountProduct: 'https://apiv18-62gh6lrj.uc.gateway.dev/api/v1/productAccount',
+    urlApiAccountTransaction: 'https://apiv18-62gh6lrj.uc.gateway.dev/api/v1/transactions',
 
-    urlApiLoan: 'http://banquito-ws-loans-zsorzzk5da-uc.a.run.app/api/v2/loans',
-    urlApiLoanProduct: 'https://banquito-ws-active-products3-zsorzzk5da-uc.a.run.app/api/v1/loanProduct',
-    urlApiLoanAmortization: 'https://banquito-ws-loans-zsorzzk5da-uc.a.run.app/api/v1/amortization',
-    urlApiLoanRepayment: 'https://banquito-ws-loans-zsorzzk5da-uc.a.run.app/api/v1/repayment',
+    urlApiLoan: 'https://apiv18-62gh6lrj.uc.gateway.dev/api/v2/loans',
+    urlApiLoanProduct: 'https://apiv18-62gh6lrj.uc.gateway.dev/api/v1/loanProduct',
+    urlApiLoanAmortization: 'https://apiv18-62gh6lrj.uc.gateway.dev/api/v1/amortization',
+    urlApiLoanRepayment: 'https://apiv18-62gh6lrj.uc.gateway.dev/api/v1/repayment',
+    apiSecurity: '?key=AIzaSyDMQIDdAhNpwjoovbocJ1xJfzzMZZqGmFY',
 };
