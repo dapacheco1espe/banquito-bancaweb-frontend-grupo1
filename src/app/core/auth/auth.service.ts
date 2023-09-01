d3bc46639a2353be7ba1a3d259fb8b2a84b6bece
Danny Pacheco <135442481+dapacheco1espe@users.noreply.github.com>
2023-09-01 14:56:16 -0500

    fix(interceptor jwt): fix jwt

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
 import { AuthUtils } from 'app/core/auth/auth.utils';
 import { UserService } from 'app/core/user/user.service';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService
@@ -73,12 +73,15 @@ export class AuthService
         {
             return throwError('User is already logged in.');
         }
-
-        return this._httpClient.post('api/auth/sign-in', credentials).pipe(
+        const cred = {
+            emailAddress:credentials.email,
+            password: credentials.password,
+        };
+        return this._httpClient.post('http://localhost:8080/login', cred, {observe: 'response'}).pipe(
             switchMap((response: any) => {
-
+                console.log(response);
                 // Store the access token in the local storage
-                this.accessToken = response.accessToken;
+                this.accessToken = response.headers.get('Authorization').split("Bearer ")[1];
 
                 // Set the authenticated flag to true
                 this._authenticated = true;
@@ -182,6 +185,6 @@ export class AuthService
         }
 
         // If the access token exists and it didn't expire, sign in using it
-        return this.signInUsingToken();
+        return of(localStorage.getItem("accessToken") !='' ? true : false);
     }
 }
